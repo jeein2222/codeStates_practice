bc82eda5d01fadfe82f6888189ce1fa164cdd2ea
jeein2222 <jiinbag057@gmail.com>
2023-02-24 12:09:02 +0900

    정적 내부 클래스

--- StaticNestedClassExample.java
+++ StaticNestedClassExample.java
@@ -1,2 +1,40 @@
-package codeStates;public class StaticNestedClassExample {
+package codeStates;
+
+/*
+    정적 내부 클래스
+ */
+
+class Outer2{
+    private int num=1;
+    private static int sNum=2;
+
+    void getPrint(){
+        System.out.println("Outer 인스턴스 메서드");
+    }
+
+    static void getPrintStatic(){
+        System.out.println("Outer 정적 메서드");
+    }
+
+    static class StaticInClass{
+        int inNum=0;
+        static int sInNum=5;
+        void test(){
+//            System.out.println("Outer num= "+num);->non-static
+            System.out.println("Outer sNum= "+sNum);
+            System.out.println("In Num= "+inNum);
+            System.out.println("In sNum= "+sInNum);
+
+//            getPrint();->non-static
+            getPrintStatic();
+        }
+
+    }
+}
+
+public class StaticNestedClassExample {
+    public static void main(String[] args) {
+        Outer2.StaticInClass s = new Outer2.StaticInClass();
+        s.test();
+    }
 }
