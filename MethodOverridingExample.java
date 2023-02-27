cbe0909c110ad44cf13eb069bbb8c783e2cb90c0
jeein2222 <jiinbag057@gmail.com>
2023-02-27 11:21:27 +0900

    methodoverriding

--- MethodOverridingExample.java
+++ MethodOverridingExample.java
@@ -1,2 +1,26 @@
-package codeStates;public class MethodOverrdingExample {
+package codeStates;
+
+class Vehicle{
+    void run(){
+        System.out.println("Vehicle is running");
+    }
+}
+
+class Bike extends Vehicle{
+    void run(){
+        System.out.println("Bike is running");
+    }
+}
+
+class MotorBike extends Vehicle{
+    void run(){
+        System.out.println("MotorBike is running");
+    }
+}
+
+
+public class MethodOverridingExample {
+    public static void main(String[] args) {
+
+    }
 }
