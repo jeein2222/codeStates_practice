43bef32fae0ee0e92020344ebf3aaaddaddf4ffb
jeein2222 <jiinbag057@gmail.com>
2023-02-28 20:04:30 +0900

    인터페이스

--- InterfaceExample1.java
+++ InterfaceExample1.java
@@ -1,2 +1,30 @@
-package codeStates;public class InterfaceExample1 {
+package codeStates;
+
+interface Cover{
+    void call();
+}
+
+class User{
+    public void callProvider(Cover provider){//Provider 클래스에 의존
+        provider.call();
+    }
+}
+class Provider1 implements Cover{
+    public void call(){
+        System.out.println("야호");
+    }
+}
+
+class Provider2 implements Cover{
+    public void call(){
+        System.out.println("무야호");
+    }
+}
+
+public class InterfaceExample1 {
+    public static void main(String[] args) {
+        User user=new User();
+        user.callProvider(new Provider1());
+        user.callProvider(new Provider2());
+    }
 }
--- InterfaceExample2.java
+++ InterfaceExample2.java
@@ -1,2 +1,41 @@
-package codeStates;public class InterfaceExample2 {
+package codeStates;
+
+interface CafeCustomer{
+    public abstract String getOrder();
+}
+
+class CafeCustomerA implements CafeCustomer{
+    @Override
+    public String getOrder() {
+        return "a glass of iced americano";
+    }
+}
+
+class CafeCustomerB implements CafeCustomer{
+    @Override
+    public String getOrder() {
+        return "a glass of strawberry latte";
+    }
+}
+
+class CafeOwner{
+    public void giveItem(CafeCustomer cafeCustomer){
+        System.out.println("Item : "+cafeCustomer.getOrder());
+    }
+}
+
+
+public class InterfaceExample2 {
+    public static void main(String[] args) {
+        CafeOwner cafeOwner = new CafeOwner();
+        CafeCustomer cafeCustomerA = new CafeCustomerA();
+        CafeCustomer cafeCustomerB = new CafeCustomerB();
+
+        cafeOwner.giveItem(cafeCustomerA);
+        cafeOwner.giveItem(cafeCustomerB);
+        /*
+            매개변수의 다형성에 의해 CafeCustomer를 통해 구현된 객체가 모두 들어올 수 있다.
+            CafeOwner 클래스가 더이상 손님의 종류에 의존적인 클래스가 아닌 독립적인 기능을 수행하는 클래스가 됨.
+         */
+    }
 }
