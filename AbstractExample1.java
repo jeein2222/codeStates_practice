e16a61010831e974d275680352e212abb9e0e225
jeein2222 <jiinbag057@gmail.com>
2023-02-28 20:04:12 +0900

    추상화

--- AbstractExample1.java
+++ AbstractExample1.java
@@ -1,2 +1,51 @@
-package codeStates;public class AbstractExample1 {
+package codeStates;
+
+abstract class Animal{//추상 클래스
+    public String kind;
+    abstract void sound();//추상 메서드
+    abstract void run();
+}
+
+class Dog extends Animal {
+    public Dog() {
+        this.kind = "포유류";
+    }
+
+    @Override
+    void sound() {
+        System.out.println("멍멍");
+    }
+
+    @Override
+    void run() {
+
+    }
+
+}
+
+class Cat extends Animal{
+    public Cat(){
+        this.kind = "포유류";
+    }
+
+    @Override
+    void sound() {
+        System.out.println("야옹");
+    }
+
+    @Override
+    void run() {
+
+    }
+
+}
+
+public class AbstractExample1 {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.sound();
+
+        Animal cat = new Cat();
+        cat.sound();
+    }
 }
