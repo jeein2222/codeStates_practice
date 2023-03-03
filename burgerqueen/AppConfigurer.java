c4990d447d2dca6da126787750021b6cfe9d433c
jeein2222 <jiinbag057@gmail.com>
2023-03-03 12:13:41 +0900

    app 의존관계 설정 클래스

--- burgerqueen/AppConfigurer.java
+++ burgerqueen/AppConfigurer.java
@@ -1,13 +1,14 @@
 package codeStates.burgerqueen;
 
 import codeStates.burgerqueen.discount.discountCondition.CozDiscountCondition;
-import codeStates.burgerqueen.discount.discountCondition.Discount;
+import codeStates.burgerqueen.discount.Discount;
 import codeStates.burgerqueen.discount.discountCondition.DiscountCondition;
 import codeStates.burgerqueen.discount.discountCondition.KidDiscountCondition;
 import codeStates.burgerqueen.discount.discountPolicy.FixedAmountDiscountPolicy;
 import codeStates.burgerqueen.discount.discountPolicy.FixedRateDiscountPolicy;
 import codeStates.burgerqueen.product.ProductRepository;
 
+//애플리케이션 의존관계 설정 클래스
 public class AppConfigurer {
     private Cart cart = new Cart(productRepository(), menu());//싱글톤 패턴
     public ProductRepository productRepository(){
