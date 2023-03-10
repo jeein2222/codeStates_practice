b701eb1ff73e51a8e5bbe185558da11c00cae023
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:10:44 +0900

    FileReader

--- inoutputstream/FileReaderExample.java
+++ inoutputstream/FileReaderExample.java
@@ -1,2 +1,23 @@
-package codeStates.inoutputstream;public class FileReaderExample {
+package codeStates.inoutputstream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderExample {
+    public static void main(String[] args) {
+        try{
+            String fileName = "C:\\javaPractice\\thisIsJava\\src\\main\\java\\codeStates\\inoutputstream\\codestates.txt";
+            FileReader file = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(file);
+
+            int data=0;
+            while((data=bufferedReader.read())!=-1){
+                System.out.println((char) data);
+            }
+            file.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
