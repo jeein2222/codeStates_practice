cae37509b29f824a3b5d278c41266185e5ba6684
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:10:02 +0900

    FileInputStream

--- inoutputstream/FileInputStreamExample.java
+++ inoutputstream/FileInputStreamExample.java
@@ -1,4 +1,24 @@
-package codeStates.inoutputStream;
+package codeStates.inoutputstream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 
 public class FileInputStreamExample {
+    public static void main(String[] args) {
+        try{
+            FileInputStream fileInputStream = new FileInputStream(
+                    "C:\\javaPractice\\thisIsJava\\src\\main\\java\\codeStates\\inoutputstream\\codestates.txt");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            int i=0;
+            while((i=bufferedInputStream.read())!=-1){
+                System.out.println((char)i);
+            }
+            fileInputStream.close();
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
