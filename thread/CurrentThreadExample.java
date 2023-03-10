75fd4565108c5b4b3be536705928d393c78c87b0
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:26:45 +0900

    스레드 인스턴스의 주소값 얻기

--- thread/CurrentThreadExample.java
+++ thread/CurrentThreadExample.java
@@ -1,2 +1,21 @@
-package codeStates.thread;public class CurrentThreadExample {
+package codeStates.thread;
+
+//스레드 인스턴스의 주소값 얻기*
+public class CurrentThreadExample {
+    public static void main(String[] args) {
+        Thread thread1=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());//Thread-0
+            }
+        });
+
+        thread1.start();
+
+        System.out.println(Thread.currentThread().getName());//main
+    }
+    /*
+        main
+        Thread-0
+     */
 }
