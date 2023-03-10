1602112a971a3271ff4329e0c108e3d28de8a4c4
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:10:16 +0900

    FileOutputStream

--- inoutputstream/FileOutputStreamExample.java
+++ inoutputstream/FileOutputStreamExample.java
@@ -1,2 +1,19 @@
-package codeStates.inoutputstream;public class FileOutputStream {
+package codeStates.inoutputstream;
+
+import java.io.FileOutputStream;
+
+public class FileOutputStreamExample {
+    public static void main(String[] args) {
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream("C:\\javaPractice\\thisIsJava\\src\\main\\java\\codeStates\\inoutputstream\\codestates2.txt");
+            String word = "code";
+
+            byte b[] = word.getBytes();
+            fileOutputStream.write(b);
+            fileOutputStream.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
 }
