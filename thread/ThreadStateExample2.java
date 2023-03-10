8fb4c5601f42726c975bc7f2522eba63bdeb8ea9
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:30:04 +0900

    스레드의 상태 변환

--- thread/ThreadStateExample2.java
+++ thread/ThreadStateExample2.java
@@ -1,2 +1,38 @@
-package codeStates.thread;public class ThreadStateExample2 {
+package codeStates.thread;
+
+//스레드의 상태 변환
+public class ThreadStateExample2 {
+    public static void main(String[] args) {
+        SumThread sumThread = new SumThread();
+
+        sumThread.setTo(10);
+
+        sumThread.start();
+
+        //메인 스레드가 sumThread의 작업이 끝날때 까지 기다림.
+        try{ sumThread.join();} catch (Exception e){}
+
+        System.out.println(String.format("1부터 %d까지의 합 : %d",sumThread.getTo(),sumThread.getSum()));
+    }
+}
+
+class SumThread extends Thread{
+    private long sum;
+    private int to;
+
+    public long getSum() {
+        return this.sum;
+    }
+
+    public int getTo() {
+        return this.to;
+    }
+
+    public void setTo(final int to) {
+        this.to = to;
+    }
+
+    public void run(){
+        for (int i = 1; i <= to; i++) sum += i;
+    }
 }
