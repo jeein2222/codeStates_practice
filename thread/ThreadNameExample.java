cf92384ed81800dad1f9a0f66b6c2ff70aa7f96e
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:28:56 +0900

    스레드 이름

--- thread/ThreadNameExample.java
+++ thread/ThreadNameExample.java
@@ -1,2 +1,24 @@
-package codeStates.thread;public class ThreadNameExample {
+package codeStates.thread;
+
+public class ThreadNameExample {
+    public static void main(String[] args) {
+        Thread thread1=new Thread(new Runnable(){
+            public void run(){
+                System.out.println("Get Thread Name");
+            }
+        });
+
+        thread1.start();
+        System.out.println("thread1.getName() : " + thread1.getName());
+
+        thread1.setName("My Thread_0");
+
+        System.out.println("thread1.getName() : " + thread1.getName());
+
+    }
+    /*
+        Get Thread Name
+        thread1.getName() : Thread-0
+        thread1.getName() : My Thread_0
+     */
 }
