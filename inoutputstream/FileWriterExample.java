b3b7ca6dafaefc0010ca62052c2ea08e0bf2df1b
jeein2222 <jiinbag057@gmail.com>
2023-03-10 15:10:59 +0900

    FileWriter

--- inoutputstream/FileWriterExample.java
+++ inoutputstream/FileWriterExample.java
@@ -1,2 +1,19 @@
-package codeStates.inoutputstream;public class FileWriterExample {
+package codeStates.inoutputstream;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterExample {
+    public static void main(String[] args) {
+        try{
+            String fileName = "C:\\javaPractice\\thisIsJava\\src\\main\\java\\codeStates\\inoutputstream\\codestates3.txt";
+            FileWriter fileWriter = new FileWriter(fileName);
+
+            String str = "written!";
+            fileWriter.write(str);
+            fileWriter.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
