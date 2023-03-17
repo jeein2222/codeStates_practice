af40288aa0cf46ddaa1aa15dc32b5c14b675aa43
jeein2222 <jiinbag057@gmail.com>
2023-03-17 15:39:20 +0900

    arraylist로 구현한 stack

--- datastructure/ArrayListStack.java
+++ datastructure/ArrayListStack.java
@@ -1,2 +1,47 @@
-package codeStates.datastructure;public class ArrayListStack {
+package codeStates.datastructure;
+
+import java.util.ArrayList;
+
+public class ArrayListStack {
+    private ArrayList<Integer> listStack = new ArrayList<>();
+
+    public void push(Integer data) {
+        listStack.add(data);
+    }
+
+    public Integer pop(){
+        if(listStack.size()==0){
+            return null;
+        }else{
+            return listStack.remove(listStack.size() - 1);
+        }
+    }
+
+    public int size(){
+        return listStack.size();
+    }
+
+    public Integer peek(){
+        if(listStack.size()==0){
+            return null;
+        }else{
+            return listStack.get(listStack.size()-1);
+        }
+    }
+
+    public String show(){
+        return listStack.toString();
+    }
+
+    public void clear(){
+        listStack.clear();
+    }
+
+    public Integer search(int data){
+        if(listStack.contains(data)){
+            return listStack.size()-listStack.indexOf(data);
+        }else{
+            return null;
+        }
+    }
 }
