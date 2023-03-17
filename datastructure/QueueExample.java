7327a47c1d1f241370f0598c8061f9d005254627
jeein2222 <jiinbag057@gmail.com>
2023-03-17 15:40:09 +0900

    queue method

--- datastructure/QueueExample.java
+++ datastructure/QueueExample.java
@@ -1,2 +1,27 @@
-package codeStates.datastructure;public class QueueExample {
+package codeStates.datastructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);//offer
+        queue.add(2);
+        queue.add(3);
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());//1 2 3
+        }
+
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+        System.out.println(queue);//[4, 5, 6]
+        System.out.println(queue.peek());//4
+        queue.remove();
+        System.out.println(queue);//[5, 6]
+
+    }
 }
