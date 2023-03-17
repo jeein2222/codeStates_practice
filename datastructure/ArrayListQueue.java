d8caec4fd4d18ba09eb1415edf8b1169e11bb61a
jeein2222 <jiinbag057@gmail.com>
2023-03-17 15:38:49 +0900

    arraylist로 구현한 queue

--- datastructure/ArrayListQueue.java
+++ datastructure/ArrayListQueue.java
@@ -1,2 +1,33 @@
-package codeStates.datastructure;public class ArrayListQueue {
+package codeStates.datastructure;
+
+import java.util.ArrayList;
+
+public class ArrayListQueue {
+    private ArrayList<Integer> listQueue = new ArrayList<>();
+
+    public void add(Integer data){
+        listQueue.add(data);
+    }
+
+    public Integer poll(){
+        if(listQueue.size()==0) return null;
+        else return listQueue.remove(0);
+    }
+
+    public int size(){
+        return listQueue.size();
+    }
+
+    public Integer peek(){
+        if(listQueue.size()==0) return null;
+        else return listQueue.get(0);
+    }
+
+    public String show(){
+        return listQueue.toString();
+    }
+
+    public void clear(){
+        listQueue.clear();
+    }
 }
