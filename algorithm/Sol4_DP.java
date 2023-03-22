68f9344f9cd0362deb2d262d37ac26b103b2e6fc
jeein2222 <jiinbag057@gmail.com>
2023-03-22 15:37:53 +0900

    금고를 털어라/동전교환 DP

--- algorithm/Sol4_DP.java
+++ algorithm/Sol4_DP.java
@@ -1,27 +1,43 @@
 package codeStates.algorithm;
 
-import java.util.Arrays;
-
-//금고를 털어라
-public class Sol4 {
-    public long ocean(int target, int[] type) {
-        //동전의 type을 정렬
-        type = Arrays.stream(type).sorted().toArray();
+//다이나믹 프로그래밍의 가장 기초적인 문제
+public class Sol4_DP {
+    //금고를 털어라
+    public long ocean(int target, int[] type){
+        long[] dp = new long[target + 1];
+        dp[0] = 1;
 
         for(int i=0;i<type.length;i++){
-            //기준이 되는 수 뽑기
-            int biggest = type[i];
-
-            int n = target / biggest;
-
-            for(int j=1;j<=n;j++){
-                int remain = target - biggest * j;
-
+            for(int j=1;j<=target;j++){
+                if(type[i]<=j)
+                    dp[j] += dp[j - type[i]];
             }
         }
-        return 1;
+        return dp[target];
     }
-    public static void main(String[] args) {
 
+    //동전교환 문제
+    public void coin(int[] coins, int target){
+        int[] dp = new int[target + 1];
+        for(int i=1;i<=target;i++) dp[i]=1000001;
+
+        for(int i=0;i<coins.length;i++){
+            System.out.printf("동전 %d 사용시\n", coins[i]);
+            for(int j=coins[i];j<=target;j++){
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                System.out.printf("%d %d\n",j,dp[j]);
+            }
+            System.out.println("-".repeat(60));
+        }
+        System.out.println(dp[target]);
+    }
+
+    public static void main(String[] args) {
+        Sol4_DP s = new Sol4_DP();
+        //금고를 털어라
+        System.out.println(s.ocean(5, new int[]{1, 2, 5}));
+
+        //동전교환 문제
+        s.coin(new int[] {1,4,5,6}, 14);
     }
 }
