6fa52d936ff6bbf3e6366c529d528ac3940edd25
jeein2222 <jiinbag057@gmail.com>
2023-03-22 15:39:47 +0900

    이진탐색트리구현

--- datastructure/BinarySearchTreeImple.java
+++ datastructure/BinarySearchTreeImple.java
@@ -1,2 +1,64 @@
-package codeStates.datastructure;public class BinarySearchTreeImple {
+package codeStates.datastructure;
+
+public class BinarySearchTreeImple {
+    public static class Node{
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(final int data) {
+            this.setData(data);
+            this.left = null;
+            this.right = null;
+        }
+
+        public int getData() {
+            return this.data;
+        }
+
+        public void setData(final int data) {
+            this.data = data;
+        }
+
+        public Node getLeft() {
+            return this.left;
+        }
+
+        public void setLeft(final Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return this.right;
+        }
+
+        public void setRight(final Node right) {
+            this.right = right;
+        }
+    }
+
+    public static class binarySearchTree{
+        public Node root;
+
+        public binarySearchTree(final Node root) {
+            this.root = root;
+        }
+
+        //tree에 value 추가
+        public void insert(int data){
+            Node newNode = new Node(data);
+            if(root ==null){ //root가 없으면
+                root = newNode;
+                return;
+            }
+            if(root.data== data) return;
+
+            Node currentNode=root;
+            Node parentNode=null;
+
+            while(true){
+                parentNode = currentNode;
+            }
+        }
+    }
 }
