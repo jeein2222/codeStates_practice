761deb9027e1f3426f377982c6d400f78519437f
jeein2222 <jiinbag057@gmail.com>
2023-04-03 15:18:35 +0900

    조합 nCr

--- algorithm/Combination.java
+++ algorithm/Combination.java
@@ -1,2 +1,43 @@
-package codeStates.algorithm;public class Combination {
+package codeStates.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+//조합 nCr
+public class Combination {
+
+    //반복문
+    public static ArrayList<String[]> combinationLoop(){
+        String[] lookup = new String[]{"A", "B", "C", "D", "E"};
+        ArrayList<String[]> result = new ArrayList<>();
+
+        //한번 조합한 요소는 다시 조합히자 않음.
+        for(int i=0;i<lookup.length;i++){
+            for(int j=i+1;j<lookup.length;j++){
+                for(int k=j+1;k<lookup.length;k++){
+                    String[] input=new String[] {lookup[i], lookup[j], lookup[k]};
+                    result.add(input);
+                }
+            }
+        }
+        return result;
+
+    }
+
+    //재귀
+    void makeCombination(char[] arr, int depth, char[] choosed, int startIdx){
+        if(depth ==choosed.length){
+            System.out.println(Arrays.toString(choosed));
+            return;
+        }
+        for(int i=startIdx;i<arr.length;i++){
+            choosed[depth] = arr[i];
+            makeCombination(arr, depth + 1, choosed, i + 1);
+        }
+    }
+    public static void main(String[] args) {
+        Combination c = new Combination();
+        c.makeCombination(new char[] { 'a','b','c'}, 0, new char[2], 0);
+    }
 }
