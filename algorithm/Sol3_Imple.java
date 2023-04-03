3150173cca1636d5d508b839797d4d9ecc873507
jeein2222 <jiinbag057@gmail.com>
2023-04-03 15:23:25 +0900

    구현_좌표

--- algorithm/Sol3_Imple.java
+++ algorithm/Sol3_Imple.java
@@ -1,12 +1,17 @@
 package codeStates.algorithm;
 
 public class Sol3_Imple {
+         /*
+            //코딩 테스트에서 좌표 인식 및 이동
+            //룩업 테이블(lookup table)
 
+
+         */
     public boolean checkBoundary(int nx, int ny, int size){
         if(nx<0 || nx>size || ny<0 || ny>size) return false;
         return true;
     }
-    public Integer boardGame(int[][] board, String operation) {
+    public Integer boardGame1(int[][] board, String operation) {
         int nx=0,ny=0;
         int size = board.length-1;
         int sum=0;
@@ -30,6 +35,10 @@ public class Sol3_Imple {
         }
         return sum;
     }
+
+
+
+
     public static void main(String[] args) {
         Sol3_Imple s = new Sol3_Imple();
         int[][] board1 = new int[][]{
@@ -38,7 +47,7 @@ public class Sol3_Imple {
                 {1, 1, 0, 0},
                 {0, 0, 0, 0}
         };
-        int output1 = s.boardGame(board1, "RRDLLD");
+        int output1 = s.boardGame1(board1, "RRDLLD");
         System.out.println(output1);
     }
 }
