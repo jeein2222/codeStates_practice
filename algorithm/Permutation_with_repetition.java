f29bc96b4f08b6bb394af5d93a286e1f9f0a338d
jeein2222 <jiinbag057@gmail.com>
2023-04-03 15:19:22 +0900

    중복 순열

--- algorithm/Permutation_with_repetition.java
+++ algorithm/Permutation_with_repetition.java
@@ -1,2 +1,46 @@
-package codeStates.algorithm;public class Permutation_with_repetition {
+package codeStates.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//중복순열 부분집합의 걔수를 구하는 경우
+/*
+    cards = {1,2,3,4} 이중에서 중복순열로 3장을 뽑는 경우의 수
+    4^3=64
+ */
+public class Permutation_with_repetition {
+
+    public ArrayList<Integer[]> pickCardsPermutationWithRepetition(int n) {
+        int[] target = {1, 2, 3, 4};
+        int[] result = new int[n];
+        ArrayList<Integer[]> list = new ArrayList<>();
+        permutation_with_repetition(0,n, target,result, list);
+        return list;
+    }
+
+    public void permutation_with_repetition(int cnt,int r,int[] target ,int[] result,ArrayList<Integer[]> list){
+        if(cnt==r){
+            int[] newResult = result.clone();
+            list.add(Arrays.stream(newResult).boxed().toArray(Integer[]::new));
+            return;
+        }
+        for(int i=0;i<target.length;i++){
+            result[cnt] = target[i];
+            permutation_with_repetition(cnt + 1, r, target, result, list);
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Permutation_with_repetition pr = new Permutation_with_repetition();
+
+        ArrayList<Integer[]> list = pr.pickCardsPermutationWithRepetition(3);
+        for(int i=0;i<list.size();i++){
+            System.out.println(Arrays.toString(list.get(i)));
+        }
+    }
+
+
 }
