6ad3bf39cb6502fed7ea189bb245e16b31aef3b3
jeein2222 <jiinbag057@gmail.com>
2023-04-15 16:36:19 +0900

    다차원 배열 정렬

--- array/multiDimensionalArraySort.java
+++ array/multiDimensionalArraySort.java
@@ -1,5 +1,26 @@
 package codeStates.array;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 //다차원 배열
-public class multiDimensionalArray {
+public class multiDimensionalArraySort {
+    public static void main(String[] args) {
+        int[][] arr = {{1, 2}, {4, 5}, {3, 4}};
+        Arrays.sort(arr, Comparator.comparingInt(o1 -> o1[0]));
+        //하나의 배열로 풀때..
+        int[] result = Arrays.stream(arr).flatMapToInt(a -> Arrays.stream(a)).toArray();
+        System.out.println(Arrays.toString(result));//[1, 2, 3, 4, 4, 5]
+
+        //두 번째 요소까지 고려할 때...
+        int[][] arr2 = {{1, 2}, {4, 5}, {3, 4}, {3, 2}};
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return Integer.compare(o1[1], o2[1]);
+            } else {
+                return Integer.compare(o1[0], o2[0]);
+            }
+        });
+    }
+
 }
